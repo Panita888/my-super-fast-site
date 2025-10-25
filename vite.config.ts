73d830a997753919525b9974048bf40f336a2594
Panita888 <156077915+Panita888@users.noreply.github.com>
2025-10-25 18:55:58 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-super-fast-site/', // This is the line to add
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
